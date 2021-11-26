9bdcc683e91259d5974aa3f086d9ec99977f29ef
Preston <preston@preston.dev>
2021-11-26 12:41:03 -0500

    More work

--- src/day12part1.rs
+++ src/day12part1.rs
@@ -1,17 +1,17 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::error::Error;
 
 use nom::{
-  IResult,
-  sequence::delimited,
-  // see the "streaming/complete" paragraph lower for an explanation of these submodules
-  character::complete::i64,
-  bytes::complete::tag,
+    bytes::complete::tag,
+    // see the "streaming/complete" paragraph lower for an explanation of these submodules
+    character::complete::i64,
+    IResult,
 };
 
-use itertools::Itertools;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+static PLANET_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 fn planet_parser(input: &str) -> IResult<&str, Planet> {
     let (input, _) = tag("<x=")(input)?;
@@ -21,16 +21,24 @@ fn planet_parser(input: &str) -> IResult<&str, Planet> {
     let (input, _) = tag(", z=")(input)?;
     let (input, z_coord) = i64(input)?;
     let (input, _) = tag(">")(input)?;
-    Ok((input, Planet {x_coord: x_coord,
-                       y_coord: y_coord,
-                       z_coord: z_coord,
-                       x_vel: 0,
-                       y_vel: 0,
-                       z_vel: 0}))
+    let result = Ok((
+        input,
+        Planet {
+            id: PLANET_ID_COUNTER.fetch_add(1, Ordering::SeqCst),
+            x_coord: x_coord,
+            y_coord: y_coord,
+            z_coord: z_coord,
+            x_vel: 0,
+            y_vel: 0,
+            z_vel: 0,
+        },
+    ));
+    result
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Planet {
+    id: usize,
     x_coord: i64,
     y_coord: i64,
     z_coord: i64,
@@ -43,37 +51,30 @@ impl Planet {
     fn from_line(line: &str) -> Planet {
         planet_parser(line).unwrap().1
     }
-}
 
-fn update_velocities(p1: &mut Planet, p2: &mut Planet) {
-    if p1.x_coord < p2.x_coord {
-        p1.x_vel += 1;
-        p2.x_coord -= 1;
-    } else if p1.x_coord > p2.x_coord {
-        p1.x_vel -= 1;
-        p2.x_coord += 1;
-    } else {
-        // x_coords are equal, velocity unchanged
-    }
-
-    if p1.y_coord < p2.y_coord {
-        p1.y_vel += 1;
-        p2.y_coord -= 1;
-    } else if p1.y_coord > p2.y_coord {
-        p1.y_vel -= 1;
-        p2.y_coord += 1;
-    } else {
-        // y_coords are equal, velocity unchanged
-    }
-
-    if p1.z_coord < p2.z_coord {
-        p1.z_vel += 1;
-        p2.z_coord -= 1;
-    } else if p1.z_coord > p2.z_coord {
-        p1.z_vel -= 1;
-        p2.z_coord += 1;
-    } else {
-        // z_coords are equal, velocity unchanged
+    fn update_velocity(&mut self, p2: &Planet) {
+        println!("Updating planet {} with planet {}", self.id, p2.id);
+        if self.x_coord < p2.x_coord {
+            self.x_coord += 1;
+        } else if self.x_coord > p2.x_coord {
+            self.x_coord -= 1;
+        } else {
+            // coords are the same, no change necessary
+        }
+        if self.y_coord < p2.y_coord {
+            self.y_coord += 1;
+        } else if self.y_coord > p2.y_coord {
+            self.y_coord -= 1;
+        } else {
+            // coords are the same, no change necessary
+        }
+        if self.z_coord < p2.z_coord {
+            self.z_coord += 1;
+        } else if self.z_coord > p2.z_coord {
+            self.z_coord -= 1;
+        } else {
+            // coords are the same, no change necessary
+        }
     }
 }
 
@@ -90,8 +91,21 @@ fn main() -> std::io::Result<()> {
         data.clear();
     }
 
-    for p in planets.iter_mut().permutations(2) {
-        update_velocities(p[0], p[1])
+    // For each planet, loop through and update its velocity with the remaining planets
+    for i in 0..planets.len() - 1 {
+        for j in i..planets.len() {
+            if i == j {
+                continue;
+            }
+            // Update the first planet with the second...
+            let mut p: Planet =  planets[i];
+            p.update_velocity(&planets[j]);
+            planets[i] = p;
+            // ...and then the second planet with the first
+            let mut p: Planet = planets[j];
+            p.update_velocity(&planets[i]);
+            planets[j] = p;
+        }
     }
 
     Ok(())
